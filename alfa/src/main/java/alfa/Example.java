603e17f3929b8a2a1b0a4d02b3fcdcbab6ccf1f4
Alex DED <alexded@yahoo.com>
2017-03-07 15:03:04 +0200

    Spring Boot : Web Application : Example

---
+++ alfa/src/main/java/alfa/Example.java
@@ -0,0 +1,21 @@
+package alfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+	
+	@RequestMapping("/")
+	String home() {
+		return "<b>Hello World!</b>";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+	}
+
+}
